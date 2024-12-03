90ae15dae4266c547fe2530000e3edb2415a5af2
Haile Jacobo Meneses Moreno <161823973+hailemen@users.noreply.github.com>
2024-12-03 12:39:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,21 +10,24 @@ with open('kmeans.pkl', 'rb') as model_file:
 with open('scaler.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
+with open('label_encoders.pkl', 'rb') as f:
+    loaded_label_encoders = pickle.load(f)
+
 # Título de la aplicación
-st.title('Identificador de Grupo de Cliente')
+st.title('Joonie, Luna & Doria Banking Group. Identificador de Clientes')
 
 # Datos demográficos del cliente
-st.header('Datos Demográficos del Cliente')
+st.header('Ingrese los datos demográficos del Cliente')
 edad = st.number_input('Edad:', min_value=18, max_value=100)
 trabajo = st.selectbox('Tipo de Trabajo:',
                         ('Administración', 'Tecnico', 'Gerencia', 'Emprendedor',
                          'Autonomo', 'Obrero', 'Servicios', 'Hogar', 'Desempleado',
-                         'Estudiante', 'Jubilado' 'Desconocido'))
+                         'Estudiante', 'Jubilado'))
 estado_civil = st.radio('Estado Civil:', ['soltero', 'casado', 'divorciado'])
 educacion = st.radio('Educación:', ['primaria', 'media', 'superior'])
 
 # Datos financieros del usuario
-st.header('Datos Financieros del Cliente')
+st.header('Ingrese los datos financieros del Cliente')
 saldo = st.number_input('Cuál es el Saldo del cliente:')
 hipoteca = st.radio('Tiene Hipoteca', ['no', 'si'])
 prestamos = st.radio('Tiene contratado prestamos:', ['no', 'si'])
@@ -51,14 +54,15 @@ user_data['educacion'] = user_data['educacion'].map({'primaria': 1, 'media': 2, 
 trabajos = {'Administración': 'profesional', 'Tecnico': 'profesional',
             'Gerencia': 'profesional', 'Autonomo': 'profesional',
             'Obrero': 'manual', 'Servicios': 'manual', 'Hogar': 'manual',
-            'Desempleado': 'otro', 'Estudiante': 'otro', 'Jubilado': 'retirado', 'Desconocido': 'otro'}
+            'Desempleado': 'otro', 'Estudiante': 'otro', 'Jubilado': 'retirado'}
 user_data['trabajo'] = user_data['trabajo'].map(trabajos).fillna('otro')
 
 categorical_features = ["trabajo", "estado_civil"]
-label_encoders = {}
 for feature in categorical_features:
-    label_encoders[feature] = LabelEncoder()
-    user_data[feature + "_encoded"] = pd.Series(label_encoders[feature].fit_transform(user_data[feature]))
+    try:
+        user_data[feature + "_encoded"] = loaded_label_encoders[feature].transform(user_data[feature])
+    except KeyError:
+        print(f"Error al codificar la característica '{feature}'. ¿El codificador está guardado correctamente?")
 
 user_data = user_data.drop(columns=categorical_features, axis=1)
 
@@ -79,10 +83,10 @@ user_data = user_data[required_columns]
 
 # Predicción
 perfil_map = {
-    0: "1: Línea de Productos Access Silver",
-    1: "2: Línea de Productos Joven Flex",
-    2: "3: Línea de Productos Access Platinum",
-    3: "4: Línea de Productos Access Starter",
+    0: "1: Línea Access: Cuentas de ahorro saldos altos, Planes de pensiones individuales, Seguros de vida o de protección familiar y Productos bancarios vinculados a la vivienda",
+    1: "2: Línea Joven Flex: Cuentas de ahorro con beneficios, Depósitos a corto plazo con flexibilidad, Préstamos personales con tasas bajas y Productos Bancarios Digitales",
+    2: "3: Línea Platinum: Fondos de inversión diversificados, Cuenta Premium con servicios exclusivos, Seguros de vida y salud de alta gama y Planes de inversión a largo plazo o jubilación privada",
+    3: "4: Línea Starter: Cuentas de ahorro con beneficios por lealtad, Microcréditos específicos, Reestructuración de deuda (hipotecas y préstamos) y Planes de ahorro para metas específicas (ejemplo: educación de hijos)",
 }
 prediction = model.predict(user_data)
 perfil = perfil_map.get(int(prediction[0]), "Perfil no encontrado")
